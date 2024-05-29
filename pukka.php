667a39801a3e3330028a5d0004cce05f774b776b
VidyaSinha <vidyasinha939@gmail.com>
2024-05-29 17:27:55 +0530

    pukka media done che

--- pukka.php
+++ pukka.php
@@ -8,11 +8,11 @@
             <div class="shape2 float-bob-x"><img src="assets/img/shape/page-header-shape2.png" alt=""></div>
             <div class="container">
                 <div class="page-header__inner">
-                    <h2>Corporate Agency</h2>
+                    <h2>Pukka Media</h2>
                     <ul class="thm-breadcrumb">
                         <li><a href="index.html"><span class="fa fa-home"></span> Home</a></li>
                         <li><i class="icon-right-arrow-angle"></i></li>
-                        <li class="color-base">Corporate Agency</li>
+                        <li class="color-base">Media Agency</li>
                     </ul>
                 </div>
             </div>
@@ -31,13 +31,14 @@
                                     <img src="assets/img/service/service-details__img6.jpg" alt="image">
                                 </div>
                                 <div class="content-box">
-                                    <h2>Corporate Agency</h2>
+                                    <h2>Pukka Media</h2>
+                                    <h4>In the heart of the digital revolution, a dream took shape.</h4></br>
                                     <p>
-                                        Applications are like the lifeblood of mobile phones and tablets today. The true
-                                        potential of you mobile phone is harnessed by applications that can provide
-                                        utility. Apps come under of the various segments. Nowad technology has become
-                                        super advanced. Even normal people are getting themselves enrolled in coding and
-                                        programming and creating applications on their own.
+                                    In the heart of the digital revolution, a dream took shape. In 2019, 
+                                    a passionate visionary, Pavak Unadkat, observed a gap in the realm of social media - 
+                                    businesses struggled to voice their uniqueness. With a blend of creativity and entrepreneurial spirit,
+                                     Pavak founded Pukka Media, not just as a media agency but as a beacon to guide brands through the dynamic 
+                                     world of social media. Then, a partnership blossomed.
                                     </p>
                                     <ul class="list-item clearfix">
                                         <li>
@@ -45,7 +46,7 @@
                                                 <span class="icon-check"></span>
                                             </div>
                                             <div class="text">
-                                                <p>Our services will feel like A mint of creativity.</p>
+                                                <p>Creative Content Development and Design.</p>
                                             </div>
                                         </li>
                                         <li>
@@ -53,7 +54,7 @@
                                                 <span class="icon-check"></span>
                                             </div>
                                             <div class="text">
-                                                <p>Our services will feel like A mint of creativity.</p>
+                                                <p>Strategic Marketing and Advertising Campaigns. </p>
                                             </div>
                                         </li>
                                         <li>
@@ -61,7 +62,7 @@
                                                 <span class="icon-check"></span>
                                             </div>
                                             <div class="text">
-                                                <p>Our services will feel like A mint of creativity.</p>
+                                                <p>Data-Driven Analytics and Insights.</p>
                                             </div>
                                         </li>
                                     </ul>
@@ -70,19 +71,14 @@
 
                             <div class="service-details__text">
                                 <div class="title">
-                                    <h3>Start with A Plan and Finish with Results</h3>
+                                    <h3>Meet the visionary Duo Behind Pukka Media's Success</h3>
                                     <p>
-                                        The magic of marketing, the science of sales. Build your brand’s voice. Grow
-                                        your brand by zeroing in on your target market. Affordable, targeted media for
-                                        everyone.
+                                    Chandni Unadkat, a marketing powerhouse, joined Pavak at Pukka Media. Their unified vision and synergy propelled the agency's innovative success.
                                     </p>
                                 </div>
                                 <div class="text">
                                     <p>
-                                        At vero eos et accusamus et iust odio dignissimos ducimus blanditiis praesentium
-                                        voluptatum deleniti atque corrupti quos dol quamolestias excepturi sint
-                                        occaecati Lead generati inbound marketing, web development, and marketing
-                                        automation solutions.
+                                    Chandni Unadkat, not just Pavak’s life partner but a powerhouse of marketing acumen, joined as the Chief Marketing Officer. Together, they became the pulse of Pukka Media. Their synergy was electric, their vision, unified. 
                                     </p>
                                 </div>
                             </div>
@@ -97,29 +93,25 @@
                                     <div class="col-xl-6 col-lg-6">
                                         <div class="content-box">
                                             <p>
-                                                Applications are like the lifeblood mobile phones and tablets today. The
-                                                true potential you mobile phone is harnessed applications that can
-                                                provide
-                                                utility. Apps come under of the various segments. Nowad technology has
-                                                become super advanced even normal people are getting themselves.
+                                            Chandni’s flair for understanding market trends and Pavak’s innovative strategies propelled Pukka Media into a league of its own. Pukka Media creates content that speaks, engages, and inspires. It’s a media agency that picks up the big boy job of advancing with time.
                                             </p>
                                             <ul class="list-item clearfix">
                                                 <li>
                                                     <div class="circle-box"></div>
                                                     <div class="text">
-                                                        <p>Our services will feel like mint of creativity.</p>
+                                                        <p>Unified Vision for Innovative Strategies.</p>
                                                     </div>
                                                 </li>
                                                 <li>
                                                     <div class="circle-box"></div>
                                                     <div class="text">
-                                                        <p>We develop application excellent technology.</p>
+                                                        <p>Content That Engages and Inspires.</p>
                                                     </div>
                                                 </li>
                                                 <li>
                                                     <div class="circle-box"></div>
                                                     <div class="text">
-                                                        <p>Practice for providing all application problems.</p>
+                                                        <p>Expertise in Market Trends and Technology.</p>
                                                     </div>
                                                 </li>
                                             </ul>
@@ -130,22 +122,22 @@
 
                             <div class="service-details__prev-next-option blog-details__prev-next-option">
                                 <div class="single-box left">
-                                    <div class="icon">
+                                    <!-- <div class="icon">
                                         <a href="#"><span class="icon-left-arrow"></span></a>
-                                    </div>
-                                    <div class="text">
+                                    </div> -->
+                                    <!-- <div class="text">
                                         <p>Prev service</p>
                                         <h3><a href="#">We grow your business</a></h3>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <div class="single-box right">
-                                    <div class="text">
+                                    <!-- <div class="text">
                                         <p>Next service</p>
                                         <h3><a href="#">Think beyond the wave</a></h3>
-                                    </div>
-                                    <div class="icon">
+                                    </div> -->
+                                    <!-- <div class="icon">
                                         <a href="#"><span class="icon-right-arrow-angle"></span></a>
-                                    </div>
+                                    </div> -->
                                 </div>
                             </div>
 
@@ -156,44 +148,49 @@
                     <div class="col-xl-4">
                         <div class="service-details__sidebar">
                             <!--Start Sidebar Single-->
-                            <div class="sidebar__single sidebar__search wow fadeInUp" data-wow-delay=".1s">
+                            <!-- <div class="sidebar__single sidebar__search wow fadeInUp" data-wow-delay=".1s">
                                 <form action="#" class="sidebar__search-form">
                                     <input type="search" placeholder="Search here...">
                                     <button type="submit"><i class="icon-magnifying-glass"></i></button>
                                 </form>
-                            </div>
+                            </div> -->
                             <!--End Sidebar Single-->
 
                             <!--Start Sidebar Single-->
                             <div class="sidebar__single sidebar__category wow fadeInUp" data-wow-delay=".3s">
                                 <div class="title-box">
-                                    <h2>Related Services</h2>
+                                    <h2>Related Ventures</h2>
                                 </div>
 
                                 <ul class="sidebar__category-list">
                                     <li>
                                         <div class="cercale-box"></div>
-                                        <a href="app-development.html">Mobile App Development<span
+                                        <a href="learneasy.php">Learneasy<span
                                                 class="icon-right-arrow-angle"></span></a>
                                     </li>
                                     <li>
                                         <div class="cercale-box"></div>
-                                        <a href="uiux-design.html">UI/UX Designer<span
+                                        <a href="pukka.php">Pukka Media<span
                                                 class="icon-right-arrow-angle"></span></a>
                                     </li>
                                     <li>
                                         <div class="cercale-box"></div>
-                                        <a href="web-development.html">Web Developer<span
+                                        <a href="meet.php">MeetEasy<span
                                                 class="icon-right-arrow-angle"></span></a>
                                     </li>
                                     <li>
                                         <div class="cercale-box"></div>
-                                        <a href="digital-marketing.html">Digital Marketing<span
+                                        <a class="active" href="intern.php">Yo Internship<span
                                                 class="icon-right-arrow-angle"></span></a>
                                     </li>
                                     <li>
                                         <div class="cercale-box"></div>
-                                        <a class="active" href="#">Corporate Agency<span
+                                        <a href="vaatche.php">Su Vaat Che<span
+                                                class="icon-right-arrow-angle"></span></a>
+                                    </li>
+                                    <li>
+                                        <div class="cercale-box"></div>
+                                        <a href="amar.php">Amar Estate Agency<span
                                                 class="icon-right-arrow-angle"></span></a>
                                     </li>
                                 </ul>
@@ -202,17 +199,14 @@
 
                             <!--Start Sidebar Single-->
                             <div class="sidebar__single sidebar__dwonlode-box wow fadeInUp" data-wow-delay=".5s">
-                                <div class="dwonlode-box">
-                                    <a href="#">
-                                        Download Doc
-                                        <i class="icon-download"></i>
-                                    </a>
-                                    <a href="#">
-                                        Download Pdf
-                                        <i class="icon-download"></i>
-                                    </a>
-                                </div>
-                            </div>
+    <div class="dwonlode-box">
+        <a href="./Pukkabro.pdf" download>
+            Download Pdf
+            <i class="icon-download"></i>
+        </a>
+    </div>
+</div>
+
                             <!--End Sidebar Single-->
 
                             <!--Start Sidebar Single-->
@@ -221,10 +215,10 @@
                                     style="background-image: url(assets/img/service/service-details-sideber__img1.jpg);">
                                 </div>
                                 <div class="content-box text-center">
-                                    <h3>Have Any Discussion<br>About Project?</h3>
+                                    <!-- <h3>Have Any Discussio<br>About Project?</h3> -->
                                     <div class="btn-box">
                                         <a class="thm-btn" href="#">
-                                            <span class="txt">Contact Us</span>
+                                            <span class="txt">Pukka Media Instagram</span>
                                             <i class="icon-next"></i>
                                         </a>
                                     </div>
